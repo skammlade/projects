9253e3f73a4d2fd8715cafe3b8334baafe38323d
Sara Kammlade <sara.kammlade@gmail.com>
2016-03-31 23:17:50 -0600

    HTTT r markdown

--- HTTT/plot.yeargender.HTTT.R
+++ HTTT/plot.yeargender.HTTT.R
@@ -1,40 +1,35 @@
-attach(HTTT)
+#load HTTT csv file
+HTTT <- read.csv("HTTT.csv")
 
-HTTT <- read.table("HTTT.csv",sep=",",header=TRUE)
-HTTT
 #load required packages
 require(ggplot2)
 require(cowplot)
 
 #create dataframe using only year and sex variables
-## @knitr variabledf
+
 df<-data.frame(table(HTTT$Year,HTTT$Sex))
 
 #barplot
-## @knitr plotp
 p <- ggplot(df, aes(Var1, Freq, group=Var2,fill=Var2))
 p <- p + geom_bar(width=0.75, 
                   position=position_dodge(width=0.78),
                   stat="identity")
 
-#plot theme
+#theme
 p <- p + theme(plot.title=element_text(family="Helvetica", 
                                        face="bold", 
                                        size=36, 
                                        color="firebrick4", 
                                        vjust=20), 
-               axis.text.x=element_text(family="Helvetica", 
-                                        face="bold",
-                                        size=18),
+               axis.text.x=element_text(family="Helvetica",
+                                        size=14, face="bold"),
                axis.title.y=element_text(margin=margin(0,17,0,0), 
-                                         family="Helvetica", 
-                                         face="bold",
-                                         size=24),
+                                         family="Helvetica",
+                                         size=14, face="bold"),
                axis.text.y=element_text(family="Helvetica", 
-                                        face="bold",
-                                        size=18), 
-               legend.text=element_text(size=18, 
-                                        face="bold"), 
+                                        size=14), 
+               legend.text=element_text(size=14, face="bold"), 
+               legend.position="bottom",
                plot.margin=unit(c(0.5,0.5,0.5,0.5), "cm"),
                axis.ticks.x=element_blank())
 
@@ -48,7 +43,7 @@ p <- p + geom_text(data=subset(df,Freq !=0),
                    aes(label=Freq), 
                    position=position_dodge(width=0.75), 
                    vjust=1.3, 
-                   size=6, 
+                   size=4.5, 
                    family="Helvetica",
                    color="white")
 
